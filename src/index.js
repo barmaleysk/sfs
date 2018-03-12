f392c0c5c151af9be3702ebb11da20b6873b17d8
vitram <vryazanow@gmail.com>
2018-03-12 22:16:52 +0300

    express2345678910

--- src/index.js
+++ src/index.js
@@ -25,12 +25,14 @@ var bodyParser = require('body-parser')
 const ejs = require('ejs')
 // set the view engine to ejs
 app.set('view engine', 'ejs')
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/../views');
 app.get('/massrass', function(req, res) {
     res.render('massrass')
 })
-app.listen(app.get('port'))
+app.listen(0, () => {
+    console.log(app.address().port)
+}
 console.log('Server is listening')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
