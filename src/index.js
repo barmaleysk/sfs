c510fb66e36e50b774287041ba8e083d7361af97
barmaleysk <43924454+barmaleysk@users.noreply.github.com>
2018-10-17 22:45:46 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ var bodyParser = require('body-parser')
 const ejs = require('ejs')
 // set the view engine to ejs
 app.set('view engine', 'ejs')
-app.set('port', process.env.PORT || 0.0.0.0);
+app.set('port', process.env.PORT || 8000);
 app.set('views', __dirname + '/../views');
 app.get('/massrass', function(req, res) {
     res.render('massrass')
